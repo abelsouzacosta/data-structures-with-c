29242a496261a763208dd0e5b39cfbd8848ca5ad
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-07-29 21:35:31 -0300

    [chore]: array

---
+++ arrays/int_array.h
@@ -0,0 +1,10 @@
+#ifndef INT_ARRAY
+
+/**
+ * @brief creates an malloced array with the size given as parameter
+ *
+ * @param size the size of the required array
+ * @return malloced array with size
+ */
+int *create_int_array_with_size(int size);
+#endif // !INT_ARRAY
