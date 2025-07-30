04597b45350edbc0fc60e7957c8e50c3ca5a6db2
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-07-29 21:37:19 -0300

    [chore]: array
    function to insert an element at a position in the array

--- arrays/int_array.h
+++ arrays/int_array.h
@@ -7,4 +7,13 @@
  * @return malloced array with size
  */
 int *create_int_array_with_size(int size);
+
+/**
+ * @brief insert the element given in the position provided
+ *
+ * @param element the element to be inserted
+ * @param position position to insert the element
+ * @param *array reference to the array
+ */
+int insert_element_at_position(int element, int position, int *array);
 #endif // !INT_ARRAY
