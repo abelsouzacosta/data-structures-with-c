2665deecec84e25b814231b32915662cae979823
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-08-07 06:47:03 -0300

    [chore]: headers
    add header for recursive function

---
+++ recursivity/headers/recursive.h
@@ -0,0 +1,10 @@
+#ifndef RECURSIVE
+#define RECURSIVE
+
+/**
+ * @brief prints the countdown starting by the number given
+ *
+ * @param number starting number
+ */
+void countdown(unsigned int number);
+#endif // !RECURSIVE
