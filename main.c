97c9b5e331eea4a0f065f3906ed2877ec49e1d55
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-08-07 07:03:30 -0300

    [chore]: code running

--- main.c
+++ main.c
@@ -1,24 +1,13 @@
-#include "arrays/headers/int_array.h"
+#include "recursivity/headers/recursive.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 int main(int argc, char *argv[]) {
-  unsigned int number_of_elements = 5;
-  int *numbers = malloc(sizeof(int) * number_of_elements);
-  if (numbers == NULL) {
-    perror("Was not possible to allocate the array");
-    return -1;
-  }
-  int original[] = {11, 23, 44, 10, 54};
-  memcpy(numbers, original, sizeof(int) * number_of_elements);
+  unsigned int user_input;
 
-  print_array(numbers, number_of_elements);
+  printf("Enter a number: ");
+  scanf("%d", &user_input);
 
-  int *new_array = pop(numbers, number_of_elements, 10);
-
-  print_array(new_array, 4);
+  countdown(user_input);
 
   return 0;
 }
