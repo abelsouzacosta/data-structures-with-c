fc8d8df6a4dc0542aaa85daac72d318c1b7bf815
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-08-14 23:07:30 -0300

    [feat](dynamic_stack): push
    implementation of function that inserts an element at the top of the
    stack by calling insert_at_head method from the linked list, then
    updates the top pointer to the head of the linked list

--- dynamic_stack/src/dynamic_stack.c
+++ dynamic_stack/src/dynamic_stack.c
@@ -23,3 +23,18 @@ DStack *initialize_stack(void) {
 bool is_empty(DStack *stack) {
   return stack->top == NULL || stack->elements->head == NULL ? true : false;
 }
+
+void push(DStack *stack, unsigned int value) {
+  if (!stack) {
+    fprintf(stderr, "No stack was provided");
+    return;
+  }
+
+  // insert an element at the head of the linked list
+  insert_at_head(stack->elements, value);
+  // the element at the top of the linked list is now the head
+  stack->top = stack->elements->head;
+
+  return;
+}
+
