13e9733143cfbbf122e6cfe986c090a78fa0f401
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-08-14 23:11:19 -0300

    [chore]: code running

--- main.c
+++ main.c
@@ -1,33 +1,11 @@
-#include "linked_list/headers/linked_list.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "dynamic_stack/headers/dynamic_stack.h"
 
 int main(int argc, char *argv[]) {
-  LinkedList *list = malloc(sizeof(LinkedList));
+  DStack *stack = initialize_stack();
+  push(stack, 2);
+  push(stack, 3);
+  push(stack, 4);
+  push(stack, 6);
 
-  insert_at_head(list, 10);
-  insert_at_head(list, 20);
-  insert_at_head(list, 30);
-  insert_at_head(list, 40);
-  insert_at_head(list, 50);
-  insert_at_tail(list, 60);
-  insert_at_tail(list, 70);
-  insert_at_tail(list, 100);
-  insert_at_head(list, 200);
-
-  insert_after_element(list, 30, 300);
-  insert_after_element(list, 100, 10000);
-
-  print_list(list);
-  printf("\nThe tail of the list is %u\n", get_tail(list));
-  printf("\nThe head of the list is %u\n", get_head(list));
-
-  // reverses the list
-  reverse_list(list);
-
-  print_list(list);
-  printf("\nThe tail of the list is %u\n", get_tail(list));
-  printf("\nThe head of the list is %u\n", get_head(list));
-
-  free(list);
+  print_stack(stack);
 }
