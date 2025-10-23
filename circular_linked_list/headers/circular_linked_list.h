a85725b5dd49334af02fdea4488ac13f5512eb3e
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-23 18:56:13 -0300

    [chore]: circular linked list
    Adding function signatures for circular linked list

--- circular_linked_list/headers/circular_linked_list.h
+++ circular_linked_list/headers/circular_linked_list.h
@@ -11,5 +11,20 @@ typedef struct CircularLinkedList {
   struct Node *tail;
 } CircularLinkedList;
 
+Node *create_node(unsigned int data);
+
+CircularLinkedList init_list();
+
+void insert_at_head(CircularLinkedList *list, unsigned int data);
+
+void insert_at_tail(CircularLinkedList *list, unsigned int data);
+
+void insert_after_element(CircularLinkedList *list, unsigned int data);
+
+void delete_at_head(CircularLinkedList *list);
+
+void delete_at_tail(CircularLinkedList *list);
+
+void delete_element(CircularLinkedList *list, unsigned int reference);
 
 #endif
