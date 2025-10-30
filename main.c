f7439b6e1a7c4f3ba5c60ce8a2d47b1dfcd2db35
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-30 18:10:12 -0300

    [chore]: LinkedList

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
+#include "linked_list/headers/linked_list.h"
 
-#include "circular_linked_list/headers/circular_linked_list.h"
 int main(int argc, char *argv[]) {
-  CircularLinkedList *list = init_list();
+  LinkedList *list = init_list();
   insert_at_head(list, 1);
   insert_at_head(list, 3);
   insert_at_head(list, 2);
+  print_list(list);
+  reverse_list(list);
   print_list(list);
   return 0;
 }
