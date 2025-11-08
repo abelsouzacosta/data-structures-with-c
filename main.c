5c9e822fe8b5fe4f92834db474da6addf512a1c2
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-11-08 17:47:37 -0300

    [chore]: LinkedList

--- main.c
+++ main.c
@@ -5,8 +5,12 @@ int main(int argc, char *argv[]) {
   insert_at_head(list, 1);
   insert_at_head(list, 3);
   insert_at_head(list, 2);
+  insert_at_tail(list, 4);
+  insert_at_tail(list, 5);
   print_list(list);
   reverse_list(list);
+  print_list(list);
+  reverse_first(list, 3);
   print_list(list);
   return 0;
 }
