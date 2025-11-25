7b0e4bffd17090bdaf50c7a38070d19a16fb7115
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-11-25 00:17:25 -0300

    [chore]: DoublyLinkedList

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
-#include "headers/linked_list.h"
+#include "headers/doubly_linked_list.h"
 
-int main() {}
+int main() {
+  DoublyLinkedList *list = init_list();
+  insert_at_head(list, 2);
+  insert_at_head(list, 3);
+  insert_at_head(list, 4);
+  insert_at_head(list, 5);
+  reverse(list);
+  print_list(list);
+  return 0;
+}
