ab316baaae9b89f45a2a47d1e784121216ae14e1
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-11-24 23:57:59 -0300

    [chore]: LinkedList

--- main.c
+++ main.c
@@ -1,14 +1,3 @@
 #include "headers/linked_list.h"
 
-int main() {
-  LinkedList *list = init_list();
-  insert_at_head(list, 1);
-  insert_at_head(list, 3);
-  insert_at_head(list, 2);
-  insert_at_tail(list, 4);
-  insert_at_tail(list, 5);
-  print_list(list);
-  remove_duplicates(list);
-  print_list(list);
-  return 0;
-}
+int main() {}
