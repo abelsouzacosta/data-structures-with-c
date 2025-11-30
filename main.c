39f6c27d4c7395bdbad1d73e5b01e6e44e3dd1f7
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-11-30 15:23:18 -0300

    [chore](dll): DoublyLinkedList

--- main.c
+++ main.c
@@ -20,16 +20,8 @@ int main(int argc, char *argv[]) {
   insert_at_head(list, 14);
   insert_at_head(list, 15);
   insert_at_head(list, 16);
-  print_list(list);
-  delete_at_head(list);
-  print_list(list);
-  delete_at_tail(list);
-  print_list(list);
-  insert_before(list, 10, 0);
-  print_list(list);
-  delete_element(list, 0);
-  print_list(list);
-  insert_at_tail(list, 1000);
+  insert_before(list, 1, 0);
+  insert_after(list, 1, 0);
   print_list(list);
 
   free(list);
