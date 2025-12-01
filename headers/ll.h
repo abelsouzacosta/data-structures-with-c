17b6348bbf27eddf1bd024f115afba453295c90d
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-12-01 16:27:59 -0300

    [chore](ll): add headers for basic functions
    Add function signatures to create list and node, to check if the list is
    valid, check if the list is empty or unary

--- headers/ll.h
+++ headers/ll.h
@@ -1,6 +1,8 @@
 #ifndef LINKEDLIST
 #define LINKEDLIST
 
+#include <sys/types.h>
+
 typedef struct LlNode {
   struct LlNode *next;
   uint data;
@@ -10,6 +12,48 @@ typedef struct SinglyLinkedList {
   struct LlNode *head;
   struct LlNode *tail;
 } Sll;
+
+/**
+ * create_list() - allocates a new singly linked list
+ *
+ * Return a malloced singly linked list
+ */
+Sll create_list(void);
+
+/**
+ * init_node() - allocates a new node
+ *
+ * @data: the value carried by the node
+ *  Return: a malloced node with next set to NULL
+ */
+LlNode init_node(uint data);
+
+/**
+ * check_list() - checks if the list is properly malloced
+ *
+ * @list: a pointer to a singly linked list
+ *
+ * Return: void
+ */
+void check_list(Sll *list);
+
+/**
+ * is_empty() - checks if the list is empty
+ *
+ * @list: a pointer to a singly linked list
+ *
+ * Return: void
+ */
+bool is_empty(Sll *list);
+
+/**
+ * is_unary() - checks if the list is unary
+ *
+ * @list: a pointer to a singly linked list
+ *
+ * Return: void
+ */
+bool is_unary(Sll *list);
 
 
 #endif
