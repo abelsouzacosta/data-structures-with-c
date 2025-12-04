fba5e4add2ff89ef93cf55f0e30626315b095d3a
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-12-04 20:38:17 -0300

    [chore](Sll): SinglyLinkedList

--- main.c
+++ main.c
@@ -2,22 +2,25 @@
 
 int main(int argc, char *argv[]) {
   Sll *list = create_list();
-
-  insert_at_head(list, 1);
-  insert_at_head(list, 2);
-  insert_at_head(list, 3);
-  insert_at_head(list, 4);
-  insert_at_head(list, 5);
-  insert_at_head(list, 6);
-  insert_at_head(list, 7);
-  insert_at_head(list, 8);
-  insert_at_head(list, 9);
-  insert_at_head(list, 10);
-  insert_at_head(list, 11);
-  insert_at_head(list, 12);
+  insert_at_tail(list, 1);
+  insert_at_tail(list, 2);
+  insert_at_tail(list, 3);
+  insert_at_tail(list, 4);
+  insert_at_tail(list, 5);
+  insert_at_tail(list, 6);
+  insert_at_tail(list, 7);
+  insert_at_tail(list, 8);
+  insert_at_tail(list, 9);
+  insert_at_tail(list, 7);
+  insert_at_tail(list, 10);
+  insert_at_tail(list, 11);
+  insert_at_tail(list, 7);
+  insert_at_tail(list, 12);
+  insert_at_tail(list, 4);
+  insert_at_tail(list, 3);
 
   print_list(list);
-  reverse_first(list, 5);
+  remove_duplicates(list);
   print_list(list);
   return 0;
 }
